ff51f8a25184358a0f7d0cfb9b470f8f0e2776e7
team-cake <eka.cheuk@gmail.com>
2020-09-04 11:30:12 +0200

    add central styles.js file

---
+++ app/styles/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		fontSize: 36,
+		marginBottom: 16,
+	},
+	androidButtonText: {
+		color: 'blue',
+		fontSize: 20,
+	},
+	textInput: {
+		height: 40,
+		width: '40%',
+		borderColor: 'gray',
+		borderWidth: 1,
+		paddingLeft: 10,
+	},
+})
