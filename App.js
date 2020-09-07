490214e70e5d3eaf569bb45dcd9f3d6fea9e43b5
team-cake <eka.cheuk@gmail.com>
2020-09-07 19:16:25 +0200

    set up missing dependencies and adjusted App.js

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 import React, { useState, useContext } from 'react'
 import AuthNav from './app/navigation/AuthNav'
 import AppNav from './app/navigation/AppNav'
-
 import { NavigationContainer } from '@react-navigation/native'
+import jwtDecode from 'jwt-decode'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
 export default function App() {
+	const [user, setUser] = useState()
 	const [token, setToken] = useState()
 
 	return (
-		<NavigationContainer>
-			{token ? <AppNav /> : <AuthNav />}
-		</NavigationContainer>
+		<Provider store={store}>
+			<NavigationContainer>
+				{token ? <AppNav /> : <AuthNav />}
+			</NavigationContainer>
+		</Provider>
 	)
 }
