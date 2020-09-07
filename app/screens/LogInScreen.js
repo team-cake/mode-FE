cbd991d23435b635c76489f6200cd3e8391ce63c
team-cake <eka.cheuk@gmail.com>
2020-09-07 23:56:11 +0200

    Adjusted navigation after log in so user will be directed to HomeScreen.

--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -22,7 +22,7 @@ export default function LogInScreen() {
 
 	useEffect(() => {
 		if (token !== null) {
-			navigation.navigate('LogInScreen')
+			navigation.navigate('Home')
 		}
 	}, [token, navigation])
 
