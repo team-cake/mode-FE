4563f3421d7ae6e5a27be932d8b29930795f1cbe
team-cake <eka.cheuk@gmail.com>
2020-09-07 19:15:47 +0200

    Set up log in and sign up screens with dispatch

--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -1,29 +1,38 @@
-import React, { useState } from 'react'
-import {
-	StyleSheet,
-	Text,
-	View,
-	Image,
-	Alert,
-	TextInput,
-	Button,
-	Platform,
-} from 'react-native'
+import React, { useState, useContext, useEffect } from 'react'
+import { Text, View, Image, TextInput, Button } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../store/user/actions'
+import { selectToken } from '../store/user/selector'
 
 import { styles } from '../styles/styles.js'
 
-export default function WelcomeScreen() {
+export default function LogInScreen() {
 	const navigation = useNavigation()
-	const [email, onChangeEmail] = useState('')
-	const [password, onChangePassword] = useState('')
+	const dispatch = useDispatch()
+	const token = useSelector(selectToken)
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	function onPress(email, password) {
+		dispatch(login(email, password))
+		setEmail('')
+		setPassword('')
+	}
+
+	useEffect(() => {
+		if (token !== null) {
+			navigation.navigate('LogInScreen')
+		}
+	}, [token, navigation])
+
 	return (
 		<>
 			<View style={styles.center}>
 				<Image source={require('../assets/mode_logo.png')} />
 				<TextInput
 					style={styles.textInput}
-					onChangeText={(text) => onChangeEmail(text)}
+					onChangeText={(text) => setEmail(text)}
 					value={email}
 					autoCapitalize='none'
 					autoCorrect={false}
@@ -32,14 +41,14 @@ export default function WelcomeScreen() {
 				/>
 				<TextInput
 					style={styles.textInput}
-					onChangeText={(text) => onChangePassword(text)}
+					onChangeText={(text) => setPassword(text)}
 					value={password}
 					autoCapitalize='none'
 					autoCorrect={false}
 					secureTextEntry={true}
 					placeholder='password'
 				/>
-				<Button title='Log In' onPress={() => console.log(email, password)} />
+				<Button title='Log In' onPress={() => onPress(email, password)} />
 				<Text
 					title='Sign Up'
 					style={styles.underline}
