a9235639d07077a1d7d406977c7973d9705cd16e
team-cake <eka.cheuk@gmail.com>
2020-09-08 10:40:34 +0200

    ensured pages can be accessed from the AuthNav.js

--- app/navigation/AuthNav.js
+++ app/navigation/AuthNav.js
@@ -2,13 +2,19 @@ import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import LogInScreen from '../screens/LogInScreen'
 import SignUpScreen from '../screens/SignUpScreen'
+import HomeScreen from '../screens/HomeScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+import DailyModeScreen from '../screens/DailyModeScreen'
 
 const Stack = createStackNavigator()
 
 const AuthNav = () => (
-	<Stack.Navigator>
+	<Stack.Navigator screenOptions={{ headerShown: false }}>
 		<Stack.Screen name='Login' component={LogInScreen} />
 		<Stack.Screen name='SignUp' component={SignUpScreen} />
+		<Stack.Screen name='Home' component={HomeScreen} />
+		<Stack.Screen name='Profile' component={ProfileScreen} />
+		<Stack.Screen name='DailyMode' component={DailyModeScreen} />
 	</Stack.Navigator>
 )
 
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View } from 'react-native'
 
 import { styles } from '../styles/styles.js'
