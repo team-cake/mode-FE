d0ea47fcafbeb1e18da47ef0f94f679ce702f72d
team-cake <eka.cheuk@gmail.com>
2020-09-08 13:31:06 +0200

    Added storage ability for the token

--- app/store/user/reducer.js
+++ app/store/user/reducer.js
@@ -1,17 +1,20 @@
 import { LOG_OUT, LOGIN_SUCCESS, TOKEN_STILL_VALID } from './actions'
+import { AsyncStorage } from 'react-native'
 
 const initialState = {
-	token: null,
-	name: null,
+	token: AsyncStorage.getItem('token'),
+	firstName: null,
 	email: null,
 }
 
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case LOGIN_SUCCESS:
+			AsyncStorage.setItem('token', action.payload.token)
 			return { ...state, ...action.payload }
 
 		case LOG_OUT:
+			AsyncStorage.removeItem('token')
 			return { ...initialState, token: null }
 
 		case TOKEN_STILL_VALID:
