48dd84c99caaa56206a9a1e0bffc670c6ca059ed
team-cake <eka.cheuk@gmail.com>
2020-09-10 00:51:58 +0200

    Made separate component for the button.

---
+++ app/Components/AppButton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { styles } from '../styles/styles.js'
+
+function AppButton({ title, onPress }) {
+	return (
+		<View style={styles.center}>
+			<TouchableOpacity
+				style={[styles.button, { backgroundColor: 'grey' }]}
+				onPress={onPress}
+			>
+				<Text style={styles.text}>{title}</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+export default AppButton
