ec35303d5389920074ff15b130ecdf7416eb94f4
team-cake <eka.cheuk@gmail.com>
2020-09-10 20:20:07 +0200

    Added new styles for card and text

--- app/styles/styles.js
+++ app/styles/styles.js
@@ -6,25 +6,6 @@ export const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
-	title: {
-		fontSize: 36,
-		marginBottom: 16,
-		textAlign: 'center',
-	},
-	header: {
-		fontSize: 25,
-		marginBottom: 16,
-		textAlign: 'center',
-	},
-	small: {
-		fontSize: 15,
-		marginBottom: 16,
-		textAlign: 'center',
-	},
-	androidButtonText: {
-		color: 'blue',
-		fontSize: 20,
-	},
 	button: {
 		borderRadius: 15,
 		justifyContent: 'center',
@@ -33,7 +14,48 @@ export const styles = StyleSheet.create({
 		width: '15%',
 		marginVertical: 3,
 	},
+	title: {
+		fontFamily: 'Roboto-Medium',
+		fontSize: 36,
+		marginBottom: 16,
+		textAlign: 'center',
+	},
+	header: {
+		fontFamily: 'Roboto-Medium',
+		fontSize: 25,
+		marginBottom: 16,
+		textAlign: 'center',
+	},
+	headerItalic: {
+		fontFamily: 'Roboto-MediumItalic',
+		fontSize: 25,
+		marginBottom: 16,
+		textAlign: 'center',
+	},
+	small: {
+		fontFamily: 'Roboto-Light',
+		fontSize: 15,
+		marginBottom: 16,
+		textAlign: 'center',
+	},
+	tinySmall: {
+		fontFamily: 'Roboto-Light',
+		fontSize: 10,
+		marginBottom: 5,
+		textAlign: 'center',
+	},
+	smallItalic: {
+		fontFamily: 'Roboto-LightItalic',
+		fontSize: 15,
+		marginBottom: 16,
+		textAlign: 'center',
+	},
+	androidButtonText: {
+		color: 'blue',
+		fontSize: 20,
+	},
 	text: {
+		fontFamily: 'Roboto-Light',
 		color: '#ffffff',
 		fontSize: 12,
 		// textTransform: 'pascalcase',
@@ -48,4 +70,20 @@ export const styles = StyleSheet.create({
 	bold: { fontWeight: 'bold' },
 	italic: { fontStyle: 'italic' },
 	underline: { textDecorationLine: 'underline' },
+	card: {
+		borderRadius: 6,
+		elevation: 3,
+		backgroundColor: '#fff',
+		shadowOffset: { width: 2, height: 2 },
+		shadowColor: '#333',
+		shadowOpacity: 0.3,
+		shadowRadius: 2,
+		marginHorizontal: 4,
+		marginVertical: 6,
+		width: '75%',
+	},
+	cardContent: {
+		marginHorizontal: 18,
+		marginVertical: 20,
+	},
 })
