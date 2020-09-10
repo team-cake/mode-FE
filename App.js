18c4815c8e8a740b70c1f334909c1a91d44fafd2
team-cake <eka.cheuk@gmail.com>
2020-09-10 20:20:44 +0200

    Added fonts to App.js

--- App.js
+++ App.js
@@ -5,15 +5,28 @@ import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
 import store from './app/store'
 import AppContainer from './app/navigation/index'
+import { useFonts } from '@use-expo/font'
+import { AppLoading } from 'expo'
 
 export default function App() {
-	return (
-		<Provider store={store}>
-			<NavigationContainer independent={true}>
-				{/* <AppNav /> */}
-				<AuthNav />
-				{/* <AppContainer /> */}
-			</NavigationContainer>
-		</Provider>
-	)
+	const [isLoaded] = useFonts({
+		'Roboto-Light': require('./app/assets/fonts/Roboto-Light.ttf'),
+		'Roboto-LightItalic': require('./app/assets/fonts/Roboto-LightItalic.ttf'),
+		'Roboto-Medium': require('./app/assets/fonts/Roboto-Medium.ttf'),
+		'Roboto-MediumItalic': require('./app/assets/fonts/Roboto-MediumItalic.ttf'),
+	})
+
+	if (!isLoaded) {
+		return <AppLoading />
+	} else {
+		return (
+			<Provider store={store}>
+				<NavigationContainer independent={true}>
+					{/* <AppNav /> */}
+					<AuthNav />
+					{/* <AppContainer /> */}
+				</NavigationContainer>
+			</Provider>
+		)
+	}
 }
