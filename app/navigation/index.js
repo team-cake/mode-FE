d855a15fee41a408431e1d3a662b5aa321f60afd
team-cake <eka.cheuk@gmail.com>
2020-09-10 04:17:57 +0200

    Fetching dailymodes data to profilescreen success

---
+++ app/navigation/index.js
@@ -0,0 +1,6 @@
+import { createAppContainer } from 'react-navigation'
+import AuthNav from './AuthNav'
+
+const AppContainer = createAppContainer(AuthNav)
+
+export default AppContainer
--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { Text, View, ScrollView } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectToken, selectUser } from '../store/user/selector'
+import { selectDailymodes } from '../store/dailymode/selector'
 
 import { styles } from '../styles/styles.js'
+import { fetchDailyModes } from '../store/dailymode/actions'
+
+import ModeCard from '../Components/ModeCard'
 
 export default function ProfileScreen() {
+	const dispatch = useDispatch()
+
+	const token = useSelector(selectToken)
+	// console.log('ProfileScreen -> token', token)
+	const dailyModes = useSelector(selectDailymodes)
+	const user = useSelector(selectUser)
+
+	const userDailymodes = dailyModes.filter((dailymode) => {
+		if (dailymode.userId === user.id) {
+			return true
+		} else {
+			return false
+		}
+	})
+
+	useEffect(() => {
+		dispatch(fetchDailyModes())
+	}, [dispatch])
+
 	return (
 		<>
-			<View style={styles.center}>
-				<Text style={styles.title}>profile</Text>
-			</View>
+			<ScrollView>
+				<View style={styles.center}>
+					<Text style={styles.title}>Profile</Text>
+					<ModeCard />
+				</View>
+			</ScrollView>
 		</>
 	)
 }
