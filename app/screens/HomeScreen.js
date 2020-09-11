3b7c238066f7830e4cc1e724d87dae3ad48b58d8
team-cake <eka.cheuk@gmail.com>
2020-09-11 04:07:54 +0200

    Possible to refresh by 'pulling' the screen down. Also set up foundation to load mode data to show in line chart

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 import { LineChart } from 'react-native-chart-kit'
 import { styles } from '../styles/styles.js'
-import { ScrollView, Text, View, Dimensions } from 'react-native'
+import {
+	ScrollView,
+	Text,
+	View,
+	Dimensions,
+	RefreshControl,
+} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch, useSelector } from 'react-redux'
 import { logOut } from '../store/user/actions'
 import { selectToken, selectUser } from '../store/user/selector'
 import AppButton from '../Components/AppButton'
+
+const wait = (timeout) => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, timeout)
+	})
+}
 
 export default function Home() {
 	const dispatch = useDispatch()
@@ -15,6 +27,20 @@ export default function Home() {
 	const token = useSelector(selectToken)
 	const user = useSelector(selectUser)
 	console.log('Home -> user', user)
+	const dailymodes = user.dailymodes
+	console.log('Home -> dailymodes', dailymodes)
+
+	// const mood = dailymodes.map((dailymode) => dailymode.mode)
+	// console.log('Home -> mood', mood)
+
+	const [refreshing, setRefreshing] = React.useState(false)
+	const onRefresh = React.useCallback(async () => {
+		setRefreshing(true)
+
+		wait(2000).then(() => setRefreshing(false))
+	}, [])
+
+	const [chartData, setChartData] = useState({})
 
 	const dataFirst = {
 		labels: [
@@ -29,12 +55,20 @@ export default function Home() {
 		datasets: [
 			{
 				data: [
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
+					// mood[1],
+					// mood[2],
+					// mood[3],
+					// mood[4],
+					// mood[5],
+					// mood[6],
+					// mood[7],
+					Math.random() * 5,
+					Math.random() * 5,
+					Math.random() * 5,
+					Math.random() * 5,
+					Math.random() * 5,
+					Math.random() * 5,
+					Math.random() * 5,
 				],
 			},
 		],
@@ -53,12 +87,13 @@ export default function Home() {
 		datasets: [
 			{
 				data: [
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
-					Math.random() * 100,
+					Math.random() * 250,
+					Math.random() * 250,
+					Math.random() * 250,
+					Math.random() * 250,
+					Math.random() * 250,
+					Math.random() * 250,
+					Math.random() * 250,
 				],
 			},
 		],
@@ -77,7 +112,12 @@ export default function Home() {
 	return (
 		<>
 			<View style={styles.center}>
-				<ScrollView>
+				<ScrollView
+					// contentContainerStyle={styles.scrollView}
+					refreshControl={
+						<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+					}
+				>
 					<Text style={styles.small}>mode</Text>
 					<AppButton title='Log out' onPress={() => onPress(logOut())} />
 					<Text style={styles.header}>Stats</Text>
@@ -85,17 +125,15 @@ export default function Home() {
 						Current Date: {moment().format('MMM Do YYYY')}{' '}
 					</Text>
 					<Text style={styles.small}>
-						Current Time: {moment().format('LTS')} 7 days ago{' '}
+						Current Time: {moment().format('LT')}{' '}
 					</Text>
-					<AppButton title='Day' />
+					{/* <AppButton title='Day' />
 					<AppButton title='Week' />
-					<AppButton title='Month' />
+					<AppButton title='Month' /> */}
 					<LineChart
 						data={dataFirst}
 						width={Dimensions.get('window').width}
 						height={220}
-						yAxisLabel='$'
-						yAxisSuffix='k'
 						yAxisInterval={1}
 						chartConfig={{
 							backgroundColor: '#000000',
@@ -124,14 +162,12 @@ export default function Home() {
 						data={dataSec}
 						width={Dimensions.get('window').width}
 						height={220}
-						yAxisLabel='$'
-						yAxisSuffix='k'
 						yAxisInterval={1}
 						chartConfig={{
 							backgroundColor: '#000000',
 							backgroundGradientFrom: '#1E2923',
 							backgroundGradientTo: '#08130D',
-							decimalPlaces: 2,
+							// decimalPlaces: 2,
 							color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
 							labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
 							style: {
@@ -149,7 +185,7 @@ export default function Home() {
 							borderRadius: 16,
 						}}
 					/>
-					<Text style={styles.small}>Your code history</Text>
+					<Text style={styles.small}>Your lines of code per day</Text>
 				</ScrollView>
 			</View>
 		</>
