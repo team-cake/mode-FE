df006f76647a139df7b289db626709e86a33ec62
team-cake <eka.cheuk@gmail.com>
2020-09-13 00:30:28 +0200

    Adjusted code to make the login work better, had to change due to changes in navigation

--- app/store/user/actions.js
+++ app/store/user/actions.js
@@ -12,10 +12,10 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const TOKEN_STILL_VALID = 'TOKEN_STILL_VALID'
 export const LOG_OUT = 'LOG_OUT'
 
-const loginSuccess = (userWithToken) => {
+export const loginSuccess = (payload) => {
 	return {
 		type: LOGIN_SUCCESS,
-		payload: userWithToken,
+		payload,
 	}
 }
 
