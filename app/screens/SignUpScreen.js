e7c9ec02a31efba09492ba21d2765b9c4ddb2ca0
team-cake <eka.cheuk@gmail.com>
2020-09-13 02:17:03 +0200

    Adjusted SignUpScreen according to the fetching on the other screens. Also adjusted navigation

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { Button, Text, TextInput, Image, View } from 'react-native'
-import { signUp } from '../store/user/actions'
-import { selectToken } from '../store/user/selector'
+import { loginSuccess } from '../store/user/actions'
 import { useDispatch, useSelector } from 'react-redux'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import DatePicker from '@react-native-community/datetimepicker'
 
 import { styles } from '../styles/styles.js'
+import axios from 'axios'
+import { apiUrl } from '../config/constants'
 
 export default function SignUpScreen() {
-	const [firstName, setFirstName] = useState('')
-	const [lastName, setLastName] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [dateOfBirth, setDateOfBirth] = useState('')
-	const [githubLink, setGithubLink] = useState('')
-
+	const [firstName, setFirstName] = useState('elo')
+	const [lastName, setLastName] = useState('elo')
+	const [email, setEmail] = useState('elo@elo.com')
+	const [password, setPassword] = useState('elo')
+	const [dateOfBirth, setDateOfBirth] = useState('1990-01-02')
+	const [githubLink, setGithubLink] = useState('elo')
+	const [showError, setShowError] = useState('')
+	const [loading, setLoading] = useState(false)
 	const dispatch = useDispatch()
-	const token = useSelector(selectToken)
+	const user = useSelector((state) => state.user)
 	const navigation = useNavigation()
 
-	function onPress(
+	async function onPress(
 		firstName,
 		lastName,
 		email,
@@ -30,27 +31,42 @@ export default function SignUpScreen() {
 		dateOfBirth,
 		githubLink
 	) {
-		dispatch(
-			signUp(firstName, lastName, email, password, dateOfBirth, githubLink)
-		)
-		setFirstName('')
-		setLastName('')
-		setEmail('')
-		setPassword('')
-		setDateOfBirth('')
-		setGithubLink('')
+		setShowError('')
+		setLoading(true)
+		const response = await axios
+			.post(`${apiUrl}/signup`, {
+				firstName,
+				lastName,
+				email,
+				password,
+				dateOfBirth,
+				githubLink,
+			})
+			.catch((err) => {
+				setLoading(false)
+				console.log('err => ', err)
+			})
+		setLoading(false)
+		if (response.data && !response.data.status) {
+			console.log('error => ', response)
+			setShowError(response.data.message)
+		} else if (response.data && response.data.status) {
+			setFirstName('')
+			setLastName('')
+			setEmail('')
+			setPassword('')
+			setDateOfBirth('')
+			setGithubLink('')
+			dispatch(loginSuccess(response.data.user))
+		}
 	}
-	// useEffect(() => {
-	// 	if (token !== null) {
-	// 		navigation.navigate('Home')
-	// 	}
-	// }, [token, navigation])
 
 	return (
 		<>
-			<KeyboardAwareScrollView>
-				<View style={styles.center}>
-					<Image source={require('../assets/mode_logo.png')} />
+			<View style={styles.center}>
+				<Image source={require('../assets/mode_logo.png')} />
+				<View style={{ height: 20 }} />
+				<View style={{ width: 250 }}>
 					<Text style={styles.title}>signup</Text>
 					<TextInput
 						style={styles.textInput}
@@ -59,6 +75,7 @@ export default function SignUpScreen() {
 						keyboardType='default'
 						placeholder='First Name'
 					/>
+					<View style={{ height: 10 }} />
 					<TextInput
 						style={styles.textInput}
 						onChangeText={(text) => setLastName(text)}
@@ -66,6 +83,8 @@ export default function SignUpScreen() {
 						keyboardType='default'
 						placeholder='Last Name'
 					/>
+					<View style={{ height: 10 }} />
+
 					<TextInput
 						style={styles.textInput}
 						onChangeText={(text) => setEmail(text)}
@@ -75,6 +94,8 @@ export default function SignUpScreen() {
 						keyboardType='email-address'
 						placeholder='Email'
 					/>
+					<View style={{ height: 10 }} />
+
 					<TextInput
 						secureTextEntry={true}
 						style={styles.textInput}
@@ -85,6 +106,8 @@ export default function SignUpScreen() {
 						secureTextEntry={true}
 						placeholder='Password'
 					/>
+					<View style={{ height: 10 }} />
+
 					<TextInput
 						style={styles.textInput}
 						onChangeText={(text) => setDateOfBirth(text)}
@@ -92,14 +115,25 @@ export default function SignUpScreen() {
 						keyboardType='number-pad'
 						placeholder='DOB - YYYY-MM-DD'
 					/>
+					<View style={{ height: 10 }} />
+
 					<TextInput
 						style={styles.textInput}
 						onChangeText={(text) => setGithubLink(text)}
 						value={githubLink}
 						placeholder='Github Handle'
 					/>
+					<View style={{ height: 5 }} />
+					<View style={{ height: 10 }}>
+						{showError.length > 0 && (
+							<Text style={{ color: 'red', fontSize: 12, textAlign: 'center' }}>
+								{showError}
+							</Text>
+						)}
+					</View>
+					<View style={{ height: 20 }} />
 					<Button
-						title='Sign up'
+						title={loading ? 'Loading...' : 'Sign up'}
 						onPress={() =>
 							onPress(
 								firstName,
@@ -112,7 +146,7 @@ export default function SignUpScreen() {
 						}
 					/>
 				</View>
-			</KeyboardAwareScrollView>
+			</View>
 		</>
 	)
 }
