6bc2b5b580c17320fa794cca99230b8e3824bf85
team-cake <eka.cheuk@gmail.com>
2020-09-14 00:07:48 +0200

    Changed constant apiUrl

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.API_URL || 'http://192.168.1.19:4000'
+export const apiUrl = process.env.API_URL || 'http://<YOUR_OWN_IPV4>:4000'
 export const DEFAULT_MESSAGE_TIMEOUT = 3000
